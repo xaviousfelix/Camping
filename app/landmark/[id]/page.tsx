b647ff1f99a5cc9387f6cdf394d5b93fe5360e4e
Nonthawat wattanawiwatwach <Nonthawat2554963@gmail.com>
2025-08-03 18:12:51 +0700

    fix: arrow function to default export

--- app/landmark/[id]/page.tsx
+++ app/landmark/[id]/page.tsx
@@ -7,33 +7,29 @@ import ShareButton from "@/components/landmark/ShareButton";
 import MapLandmark from "@/components/map/MapLandmark";
 import { redirect } from "next/navigation";
 
-const LandmarkDetail = async ({ params }: { params: { id: string } }) => {
-  const { id } = params; 
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
   const landmark = await fetchLandmarkDetail({ id });
 
   if (!landmark) redirect("/");
+
   return (
     <section>
       <Breadcrums name={landmark.name} />
       <header className="flex justify-between mt-4 items-center">
-        <h1 className="text-4xl font-bold"> {landmark.name}</h1>
+        <h1 className="text-4xl font-bold">{landmark.name}</h1>
         <div className="flex items-center gap-x-4">
-          <ShareButton landmarkId={landmark.id} name={landmark.name}/>
+          <ShareButton landmarkId={landmark.id} name={landmark.name} />
           <FavoriteToggleButton landmarkId={landmark.id} />
         </div>
       </header>
-      {/* Image */}
       <ImageContainer mainImage={landmark.image} name={landmark.name} />
-      {/* Detail */}
       <section>
         <div>
           <Description description={landmark.description} />
-          <MapLandmark 
-          location={{ lat: landmark.lat, lng: landmark.lng }} 
-          />
+          <MapLandmark location={{ lat: landmark.lat, lng: landmark.lng }} />
         </div>
       </section>
     </section>
   );
-};
-export default LandmarkDetail;
+}
