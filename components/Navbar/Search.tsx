4241ec1ca96ecf2fde013568040882ba41b40154
Nonthawat wattanawiwatwach <Nonthawat2554963@gmail.com>
2025-08-03 19:59:52 +0700

    add suspense

--- components/Navbar/Search.tsx
+++ components/Navbar/Search.tsx
@@ -3,7 +3,6 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { Input } from "../ui/input";
 import { useEffect, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
-import { Suspense } from "react";
 
 const Search = () => {
   const searchParams = useSearchParams();
@@ -23,25 +22,22 @@ const Search = () => {
   }, 500);
 
   useEffect(() => {
-    // code body
     if (!searchParams.get("search")) {
       setSearch("");
     }
   }, [searchParams.get("search")]);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Input
-        type="text"
-        placeholder="Seach Camping..."
-        className="max-w-xs"
-        onChange={(e) => {
-          setSearch(e.target.value);
-          handleSearch(e.target.value);
-        }}
-        value={search}
-      />
-    </Suspense>
+    <Input
+      type="text"
+      placeholder="Search Camping..."
+      className="max-w-xs"
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
+    />
   );
 };
 export default Search;
